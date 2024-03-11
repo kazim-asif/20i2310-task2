da067576c44f6f3c75ad9e77c30ff9af8208ec85
Kazim786-dev <kazimasif2020@gmail.com>
2024-03-11 10:40:47 +0500

    add test to enroll after drop

--- test.py
+++ test.py
@@ -12,6 +12,11 @@ def test_drop_students():
     students.dropStudents(5)
     assert students.getTotalStrength() ==  5
 
+def test_enroll_students_after_drop():
+    students = StudentsInMLOps()
+    students.enrollStudents(10)
+    assert students.getTotalStrength() ==  10
+
 def test_get_class_name():
     students = StudentsInMLOps()
     assert students.getClassName() == 'StudentsInMLOps'
